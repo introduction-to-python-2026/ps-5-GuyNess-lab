2689c01f23b487f31d86d502e5b9eea3238184bc
Guy Ness <guyness03@gmail.com>
2025-11-24 18:09:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import string_utils as su
 import equation_utils as eu
 
+def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
-def balance_reaction(reaction):
-    # 1. ניתוח התגובה (Parsing)
-    reactants, products = su.parse_chemical_reaction(reaction)
-    reactant_atoms = su.count_atoms_in_reaction(reactants)
+    # 1.parse reaction
+    reactants, products = su.parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
+    reactant_atoms = su.count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
     product_atoms = su.count_atoms_in_reaction(products)
 
-    # 2. בניית משוואה ופתרון (Building and Solving)
-    # eu.build_equations מחזיר את כל המשוואות ואת הסמלים שצריך לפתור
-    equations, coefficients_symbols = eu.build_equations(reactant_atoms, product_atoms)
-    
-    # eu.my_solve פותר את המערכת. מוסיפים [1] עבור המקדם האחרון
-    unsimplified_coefficients = eu.my_solve(equations, coefficients_symbols) + [1]
-    
-    # 3. פישוט המקדמים למספרים שלמים (השלמה חיונית)
-    # eu.simplify_coefficients אמורה להמיר את השברים למספרים שלמים
-    final_coefficients = eu.simplify_coefficients(unsimplified_coefficients)
+    # 2.build equation and solve
+    equations, coefficients = eu.build_equations(reactant_atoms, product_atoms)
+    coefficients = eu.my_solve(equations, coefficients) + [1]
 
-    # 4. עיצוב הפלט הסופי כמחרוזת (השלמה חיונית)
-    # eu.format_balanced_reaction מחברת את המקדמים השלמים עם המולקולות
-    balanced_equation = eu.format_balanced_reaction(reactants, products, final_coefficients)
-    
-    # מחזירים את המשוואה המאוזנת כמחרוזת
-    return balanced_equation
+    return coefficients # [1/3, 1, 2/3, 1]
+
+
