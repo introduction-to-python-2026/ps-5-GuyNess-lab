d5e40b2d172df3e7d6f6fa1444e1fc2c1a85a5a5
Guy Ness <guyness03@gmail.com>
2025-11-24 16:48:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-from string_utils import (
-    parse_chemical_reaction,
-    count_atoms_in_reaction
-)
-
-from equation_utils import (
-    build_equations,
-    my_solve,
-    simplify_coefficients,
-    format_balanced_reaction
-)
+import string_utils as su
+import equation_utils as eu
 
 def balance_reaction(reaction):
-    reactants, products = parse_chemical_reaction(reaction)
-    reactant_atoms = count_atoms_in_reaction(reactants)
-    product_atoms = count_atoms_in_reaction(products)
+    """
+    Balances a chemical reaction equation using linear algebra.
+    Example: 'Fe2O3 + H2 -> Fe + H2O'
+    """
+    # 1. ניתוח התגובה
+    reactants, products = su.parse_chemical_reaction(reaction)
+    reactant_atoms = su.count_atoms_in_reaction(reactants)
+    product_atoms = su.count_atoms_in_reaction(products)
 
-    equations, coefficient_symbols = build_equations(reactant_atoms, product_atoms)
+    # 2. בניית ופתרון המערכת
+    equations, coefficients_symbols = eu.build_equations(reactant_atoms, product_atoms)
     
-    unsimplified_coefficients = my_solve(equations, coefficient_symbols) + [1]
+    # 3. מציאת מקדמים (הוספת [1] למקדם האחרון)
+    # eu.my_solve אמור להחזיר רשימת פתרונות (שברים/עשרוניים)
+    unsimplified_coefficients = eu.my_solve(equations, coefficients_symbols) + [1]
     
-    final_coefficients = simplify_coefficients(unsimplified_coefficients)
+    # 4. פישוט המקדמים למספרים שלמים
+    # eu.simplify_coefficients אמור להמיר את השברים למספרים שלמים
+    final_coefficients = eu.simplify_coefficients(unsimplified_coefficients)
 
-    balanced_equation = format_balanced_reaction(
-        reactants, products, final_coefficients
-    )
+    # 5. עיצוב הפלט הסופי כמחרוזת
+    # eu.format_balanced_reaction אמור לחבר את המקדמים והמולקולות
+    balanced_equation = eu.format_balanced_reaction(reactants, products, final_coefficients)
     
     return balanced_equation
 
+# קוד הרצה מקומי לבדיקה (אם צריך)
 def main():
     reaction = "Fe2O3 + H2 -> Fe + H2O"
     balanced_eq = balance_reaction(reaction)
